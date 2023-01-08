39742f61eb0089de36f5c0449d76dedc2d3b1a7e
Jam Jam James Perey <jamjam.perey@gmail.com>
2023-01-08 12:17:29 -0800

    UPDATE: added RecursiveDigitSum template

---
+++ Templates/T-RecursiveDigitSum.py
@@ -0,0 +1,24 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+
+def superDigit(n, k):
+    # copy to your preferred IDE and code there and then copy + paste into HackerRank once solution is found
+    pass
+
+
+response = superDigit("123", 3)
+
+print(f"response: {response}")
